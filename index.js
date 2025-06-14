86f4463070453ba006ef59264d9f0056ae7dc411
brench maina <bein67544@gmail.com>
2025-06-14 19:00:25 +0300

    dom lab done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// // Write your code here!
+
+//remove the <main> element
+document.querySelector('main').remove();
+
+// create a new <h1> element
+const newHeader = document.createElement('h1');
+
+//set the newHeader to 'victory'
+newHeader.id = 'victory';
+
+//addan innerHTML
+newHeader.innerHTML= 'Brench is the champion';
+
+//add the newHeader to the body
+document.body.appendChild(newHeader);
